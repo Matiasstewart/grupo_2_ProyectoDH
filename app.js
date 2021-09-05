81a60b9c7b104b0cbcbaa535d7e0988b19b89992
LuchoAiello <luchoaiello1@gmail.com>
2021-09-05 16:29:06 -0300

    Creando el register

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const publicPath = path.resolve(__dirname, './public')
 
 app.use(express.static(publicPath));
 
+app.listen(3090, () => {
+    console.log('Servidor corriendo en el puerto http://localhost:3090/ !');
+});
+
 app.get('/', (req, res) => {
     res.sendFile (path.resolve (__dirname , 'views/index.html'))
 });
 
-app.listen(3090, () => {
-    console.log('Servidor corriendo en el puerto http://localhost:3090/ !');
+app.get('/register', (req, res) => {
+    res.sendFile (path.resolve (__dirname , 'views/register.html'))
 });
