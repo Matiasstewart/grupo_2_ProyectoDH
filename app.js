cf39a9e39516d530b55de6167d27a4633021a733
LuchoAiello <luchoaiello1@gmail.com>
2021-09-05 11:59:48 -0300

    cambios en app,.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const public = path.resolve(__dirname, './public')
+const publicPath = path.resolve(__dirname, './public')
 
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-    res.senFile (path.resolve (__dirname , 'views/index.html'))
+    res.sendFile (path.resolve (__dirname , 'views/index.html'))
 });
 
 app.listen(3090, () => {
