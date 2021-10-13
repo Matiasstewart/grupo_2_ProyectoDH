255990274b3e9ffeba54d3686c04fcbdb60d76c7
galvancamilanicole <galvancamilanicole@gmail.com>
2021-10-13 16:05:40 -0300

    cambios en products.js

--- routers/products.js
+++ routers/products.js
@@ -1,9 +1,24 @@
 let express = require("express");
+const multer = require("multer");
+const path = require("path")
 const { carrito } = require("../controllers/productsController");
 let router = express.Router();
 
 // CONTROLADORES
 let productsController = require("../controllers/productsController")
+
+// MULTER
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) {
+       cb(null, './public/images');
+    },
+    filename: function (req, file, cb) {
+        const newFileName = 'product_' + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);
+    }
+})
+
+const upload = multer({storage: storage});
 
 // Listado
 router.get("/",productsController.list)
@@ -21,5 +36,8 @@ router.post("/", productsController.store)
 // Editar y almacenar
 router.get("/editar/:id",productsController.edit)
 router.put("/", productsController.update)
+
+// Borrar
+router.delete("/detalle/:id",productsController.delete)
 
 module.exports = router
