a62834788b2f5d817e2e87963f5730dc1a0617d2
LuchoAiello <luchoaiello1@gmail.com>
2021-10-14 22:18:52 -0300

    agregue users.json, hice funcinal el register, encrypte las password

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+
+const userJson = path.join(__dirname, '../data/user.json');
+const users = JSON.parse(fs.readFileSync(userJson, 'utf-8'));
+
 const usersController = {
     login: (req,res)=>{
         res.render('users/login')
@@ -5,6 +12,23 @@ const usersController = {
     registro: (req,res)=>{
         res.render('users/register')
     },
+    proccesRegister: (req,res) => {
+            const newUsers = {
+                id: users[users.length -1].id + 1,
+                name: req.body.name,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                img: req.file.filename,
+                category: req.body.category,
+                password: bcrypt.hashSync(req.body.psw, 10),
+            }
+    
+            users.push(newUsers);
+            fs.writeFileSync(userJson, JSON.stringify(users,null, 4))
+            res.redirect("/usuario/login")
+            
+       }
+
 }
 
 module.exports = usersController;
--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,25 @@
-let express = require("express");
-let router = express.Router();
+const express = require("express");
+const router = express.Router();
+const path = require('path')
+const multer = require('multer');
+
+//para definimos donde se van a guardar las imagenes de los usuarios
+const storage = multer.diskStorage({ 
+   destination: function (req, file, cb) { 
+      cb(null, './public/images/avatars'); 
+   }, 
+   filename: function (req, file, cb) { 
+      cb(null, Date.now() + path.extname(file.originalname));  } 
+ });
+
+const uploadFile = multer({ storage });
 
 // CONTROLADOR
 let usersController = require("../controllers/usersController");
 
 router.get("/login",usersController.login);
 
-router.get("/registro",usersController.registro);
+router.get("/registro", usersController.registro);
+router.post("/login", uploadFile.single('image'),usersController.proccesRegister)
 
 module.exports = router;
