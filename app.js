fe62edad2dd38346780192da77dbfc022c237b26
galvancamilanicole <galvancamilanicole@gmail.com>
2021-11-03 16:26:17 -0300

    agregue paquetes session y cookies

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 const methodOverride = require('method-override');
 
+// Session y cookies
+const session = require('express-session');
+const cookies = require('cookie-parser');
+
+app.use(session({secret:'Secreto!', resave:false, saveUninitialized: false}));
+app.use(cookies());
+
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
+app.use(userLoggedMiddleware);
+// 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride('_method')); 
