0cdf403a9fffe994137c10c8fe622c7cf66f2ce4
Matiasstewart <matiasstewartusher@hotmail.com>
2021-11-28 18:53:41 -0300

    cree el modelo de size

---
+++ src/database/models/Size.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Sizes';
+    let cols = {
+       id: {
+            type: dataTypes.INTEGER(11),
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+       },
+       size: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+       }
+    };
+   
+    let config = {
+        tableName: 'sizes',
+        timestamps: false
+    }
+   
+     const Size = sequelize.define(alias, cols, config); 
+
+     Size.associate = function (models){
+        Size.belonsToMany(models.Product, {
+         as:"sizes",
+         through: "products_sizes",
+         foreingKey: "size_id",
+         otherKey:"product_id",
+         timestamps:"false"   
+        })
+    }
+   }
