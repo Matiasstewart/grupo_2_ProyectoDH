d08c19c7deaacb1492ad58c83eef93d6feaa4dbd
Matiasstewart <matiasstewartusher@hotmail.com>
2021-11-28 21:16:33 -0300

    cree el modelo de category

---
+++ database/models/Category.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Category';
+    let cols = {
+      id: {
+          type: dataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      category: {
+          type: dataTypes.STRING(100),
+          allowNull: false
+      }
+    }
+    let config = {
+        tableName: 'categories',
+        timestamps: false
+    }
+
+    const Category = sequelize.define(alias, cols, config);
+
+    Category.associate = function (models){
+        Category.hasMany(models.Product, {
+            foreignkey: 'category_id',
+            as:'products'
+        })
+    }
+
+    return Category
+}
