8cd4132c6f89ea8aee1e9bc81586d483edd80b14
Matiasstewart <matiasstewartusher@hotmail.com>
2021-11-28 21:28:59 -0300

    agcree el modelo de season

---
+++ database/models/Season.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Season';
+    let cols = {
+      id: {
+          type: dataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      season: {
+          type: dataTypes.STRING(20),
+          allowNull: false
+      }
+    }
+    let config = {
+        tableName: 'seasons',
+        timestamps: false
+    }
+
+    const Season = sequelize.define(alias, cols, config);
+
+    Season.associate = function (models){
+        Season.hasMany(models.Product, {
+            foreignkey: 'season_id',
+            as:'products'
+        })
+    }
+
+    return Season
+}
