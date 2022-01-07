7a0d753bcb8131225515a7ccf7483d051e30384a
LuchoAiello <luchoaiello1@gmail.com>
2022-01-07 15:29:22 -0300

    burgerMenu funcionando

--- public/js/main.js
+++ public/js/main.js
@@ -33,3 +33,16 @@ let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
         track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
     }
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
