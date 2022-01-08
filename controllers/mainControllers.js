4dbdf265bdbb29ec213f9078d6915cf0f89030ff
galvancamilanicole <galvancamilanicole@gmail.com>
2022-01-08 11:30:38 -0300

    cambios index

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,7 +1,20 @@
+const db = require('../database/models');
+const { Op } = require("sequelize");
+
 const mainController = {
-    index: (req,res) => {
-        res.render('index')
+    index: (req,res)=>{
+        db.Product.findAll({
+            where:{
+                discount:{
+                    [Op.or]:[{a:10},{a:20}]
+                }
+            },
+            limit:8
+        }).then(products =>{
+            return res.render('index',{products})
+        })
     }
+    
 }
 
 module.exports = mainController;
