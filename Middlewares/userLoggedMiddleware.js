d6f5b5fac284268d7a3aa22d430645677ab5fb81
galvancamilanicole <galvancamilanicole@gmail.com>
2022-01-09 07:00:02 -0300

    cambios cookies y vista lista de productos

--- Middlewares/userLoggedMiddleware.js
+++ Middlewares/userLoggedMiddleware.js
@@ -1,22 +1,37 @@
 const fs = require('fs');
 const path = require("path");
-const userJson = path.join(__dirname, '../data/user.json');
-const users = JSON.parse(fs.readFileSync(userJson, 'utf-8'));
+const db = require("../database/models")
+// const userJson = path.join(__dirname, '../data/user.json');
+// const users = JSON.parse(fs.readFileSync(userJson, 'utf-8'));
 
 function userLoggedMiddleware(req, res, next) {
 	res.locals.isLogged = false;
 
-	let userFromCookie = users.find(user => user.email === req.cookies.userEmail);
-
-	if (userFromCookie) {
-		req.session.userLogged = userFromCookie;
-	}
+	let emailCookie = req.cookies.userEmail;
 
 	if (req.session.userLogged) {
 		res.locals.isLogged = true;
 		res.locals.userLogged = req.session.userLogged;
 	}
 
+	if(req.cookies.userEmail){
+		db.User.findOne({
+			where:{
+				email: emailCookie
+			}
+		})
+		.then(userFromCookie =>{
+			if(userFromCookie){
+				req.session.userLogged = userFromCookie;
+				res.locals.isLogged = true;
+				res.locals.userLogged = req.session.userLogged;
+			}
+	
+		})
+	}
+
+	
+	
 	next();
 }
 
